4e6dfab1373dad9e93b0353592da0b32b9015658
d3x0r <d3x0r@no-reply.github.com>
2020-09-08 21:14:18 -0700

    Add missing server?

---
+++ server/testWsHttp.mjs
@@ -0,0 +1,82 @@
+
+import sack from "sack.vfs";
+
+//var sack = require( "sack.vfs" );
+import path from "path";
+//const path = require( "path" );
+var serverOpts;
+var server = sack.WebSocket.Server( serverOpts = { port: Number(process.argv[2])||8080 } )
+var disk = sack.Volume();
+console.log( "serving on " + serverOpts.port );
+
+
+server.onrequest( function( req, res ) {
+	var ip = ( req.headers && req.headers['x-forwarded-for'] ) ||
+		 req.connection.remoteAddress ||
+		 req.socket.remoteAddress ||
+		 req.connection.socket.remoteAddress;
+	//ws.clientAddress = ip;
+
+	console.log( "Received request:", req.url );
+	if( req.url === "/" ) req.url = "/index.html";
+	var filePath = "." + unescape(req.url);
+	var extname = path.extname(filePath);
+	var contentType = 'text/html';
+	//console.log( ":", extname, filePath )
+	switch (extname) {
+		  case '.js':
+		  case '.mjs':
+			  contentType = 'text/javascript';
+			  break;
+		  case '.css':
+			  contentType = 'text/css';
+			  break;
+		  case '.json':
+			  contentType = 'application/json';
+			  break;
+		  case '.png':
+			  contentType = 'image/png';
+			  break;
+		  case '.jpg':
+			  contentType = 'image/jpg';
+			  break;
+		  case '.wav':
+			  contentType = 'audio/wav';
+			  break;
+                case '.crt':
+                        contentType = 'application/x-x509-ca-cert';
+                        break;
+                case '.pem':
+                        contentType = 'application/x-pem-file';
+                        break;
+                  case '.wasm': case '.asm':
+                  	contentType = 'application/wasm';
+                        break;
+	}
+	if( disk.exists( filePath ) ) {
+		res.writeHead(200, { 'Content-Type': contentType });
+		//console.log( "Read:", "." + req.url );
+		res.end( disk.read( filePath ) );
+	} else {
+		console.log( "Failed request: ", req );
+		res.writeHead( 404 );
+		res.end( "<HTML><HEAD>404</HEAD><BODY>404</BODY></HTML>");
+	}
+} );
+
+server.onaccept( function ( ws ) {
+	this.accept();
+} );
+
+server.onconnect( function (ws) {
+	console.log( "Connect:", ws );
+
+	ws.onmessage( function( msg ) {
+        	//console.log( "Received data:", msg );
+                ws.send( msg );
+		//ws.close();
+        } );
+	ws.onclose( function() {
+        	//console.log( "Remote closed" );
+        } );
+} );
