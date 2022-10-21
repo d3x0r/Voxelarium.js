f9ab957d0606ebdfe4929cc0e97572c621a9baaa
d3x0r <d3x0r@no-reply.github.com>
2022-10-20 23:45:44 -0700

    Update to common server code; attempt to bring in login server locally if not available

--- server/server.mjs
+++ server/server.mjs
@@ -1,15 +1,26 @@
 // mostly just a copy of sack.vfs/tests/testWsHttp.js
 
 import {sack} from "sack.vfs"
+import config from "../config.jsox"
 import path from "path";
 
 import {db} from "./db.mjs"
-
+import {openServer} from "sack.vfs/apps/http-ws";
+console.log( "ya?", openServer );
 const AsyncFunction = Object.getPrototypeOf( async function() {} ).constructor;
 
-const loginServiceSource = sack.HTTP.get( {method:"GET",hostname:"localhost", port:8089, path:"/serviceLogin.mjs" } );
-const serviceLoginInit = new AsyncFunction( "Import", loginServiceSource.content );
-const loginService = serviceLoginInit.call( this, (i)=>import(i) );
+const loginServiceSource = sack.HTTP.get( {method:"GET",hostname:config.login.host, port:config.login.port, path:config.login.script } );
+const serviceLoginInit = !loginServiceSource.error && new AsyncFunction( "Import", loginServiceSource.content );
+console.log( "code:", loginServiceSource );
+let loginService = serviceLoginInit && serviceLoginInit.call( this, (i)=>import(i) );
+
+if( !loginService ) {
+	process.env.LOGIN_PORT = config.login.port;
+	 const udb = await import( "@d3x0r/user-database/service" );
+	console.log( "Got?", udb );
+//		udb.server
+}
+else
 
 loginService.then( (loginService)=>{
 	loginService.open( "org.d3x0r.games", "Voxelarium", {
@@ -24,98 +35,17 @@ loginService.then( (loginService)=>{
 //import {UserDb} from "http://localhost:8089/serviceLogin.js";
 
 
-var serverOpts;
-var server = sack.WebSocket.Server( serverOpts = { port: Number(process.argv[2])||8080 } )
-var disk = sack.Volume();
-console.log( "serving on " + serverOpts.port );
+let serverOpts;
+const server = openServer(  serverOpts = { //npmPath:"../"
+		//, resourcePath:".."
+		port: Number(process.env.PORT) || Number(process.argv[2])||8080 }
+		, accept, connect );
+const disk = sack.Volume();
+console.log( "voxelarium serving on " + serverOpts.port );
 
 db.init( );
 
-const pending = [];
-let busy = 0;
-function tickPendingMsg() {
-	if( pending.length ) {
-        	if( busy != pending.length ) {
-                	busy = pending.length
-                } else {
-	        	const msg = pending.join( ", " );
-        	        console.log( "Requests: ", msg );
-        		pending.length = 0;
-                }
-        }
-        setTimeout( tickPendingMsg, 500 );
-}
-tickPendingMsg();
-
-
-
-const mimeTypes = {
-	'.js'  :'text/javascript',
-	'.mjs' :'text/javascript',
-	'.css' :'text/css',
-	'.json':'application/json',
-	'.png' :'image/png',
-	'.jpg' :'image/jpg',
-	'.wav' :'audio/wav',
-	'.crt' :'application/x-x509-ca-cert',
-	'.pem' :'application/x-pem-file',
-	'.wasm':'application/wasm',
-	'.asm' :'application/wasm',
-};
-
-
-
-server.onrequest( function( req, res ) {
-	/*
-	const ip = ( req.headers && req.headers['x-forwarded-for'] ) ||
-		 req.connection.remoteAddress ||
-		 req.socket.remoteAddress ||
-		 req.connection.socket.remoteAddress;
-	*/
-	//ws.clientAddress = ip;
-
-	pending.push( req.url );
-
-        const parts = unescape(req.url).split('/');
-        if( parts.length == 2 ) {
-        }else if( parts.length > 2 ) {
-            if( parts[1] === 'node_modules' ) {
-                switch( parts[2] ) {
-                case "@d3x0r":
-                case "jsox":
-                	break;
-		default:
-			res.writeHead( 404 );
-			res.end( "<HTML><HEAD><title>404</title></HEAD><BODY>404</BODY></HTML>");
-                        return;
-                }
-            }
-        }
-        if( parts[parts.length-1] == "" ) {
-            parts[parts.length-1] = 'index.html';
-        }
-	//console.log( "Received request:", req.url );
-	//if( req.url === "/" ) req.url = "/index.html";
-	
-	const filePath = "." + parts.join('/');
-	const extname = path.extname(filePath);
-	var contentType = 'text/html';
-	//console.log( ":", extname, filePath )
-        if( extname in mimeTypes )
-            contentType = mimeTypes[extname];
-
-	if( disk.exists( filePath ) ) {
-		res.writeHead(200, { 'Content-Type': contentType });
-		//console.log( "Read:", "." + req.url );
-		res.end( disk.read( filePath ) );
-	} else {
-		console.log( "Failed request: ", req.url );
-		res.writeHead( 404 );
-		res.end( "<HTML><HEAD><title>404</title></HEAD><BODY>404</BODY></HTML>");
-	}
-} );
-
-server.onaccept( function ( ws ) {
+function accept( ws ) {
 	const protocol = ws.headers['Sec-WebSocket-Protocol'];
 	console.log( "Connection received with : ", protocol );
 	if( protocol === "VOXDB" ) {
@@ -124,9 +54,9 @@ server.onaccept( function ( ws ) {
 		this.reject();
         }
 	return;
-} );
+};
 
-server.onconnect( function (ws) {
+function connect( ws ) {
 	//const protocol = ws.headers['Sec-WebSocket-Protocol'];
 	db.connect( ws );
-} );
+};
