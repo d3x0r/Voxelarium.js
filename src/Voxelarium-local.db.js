0c31995a2d1142225ef81a3812a9e5968208f85f
d3x0r <d3x0r@no-reply.github.com>
2023-04-21 01:26:27 -0700

    Add local controls and db

---
+++ src/Voxelarium-local.db.js
@@ -0,0 +1,349 @@
+
+import {Voxelarium} from "./Voxelarium.core.js"
+
+
+import {JSOX} from "../common/JSOX.js"
+
+const l = {
+	playerId : localStorage.getItem( "playerId" ),
+	serviceId : null,
+	requiredLoad : 0,
+}
+
+function makeSocket( addr, protocol ) {
+	try {
+	const ws = new WebSocket( addr, protocol
+			,{
+				perMessageDeflate: false,
+				//ca : config.caRoot
+			}
+		);
+	return ws;
+	}catch(err) {
+		console.log( "Is this errror?", err );
+	}
+}
+
+class Db  {
+	websocket = null;
+	onComplete = null;
+	connected = true;
+	events = {};
+	on(a,b){
+		if( "function" === typeof b ) {
+			events[a] = b;
+		} else {
+			events[a](b);
+		}
+	}
+	animate() {
+		// called every half second during animation loop
+		// this allows sending/retreiving external player positions.
+	}
+	player = {
+		positionUpdate : false,
+	        name: "Player One",
+		
+		setName(name) {
+                	db.websocket.setName( name );
+                }
+                , events_ : {}
+		, on(event,arg) {
+			if( "function" === typeof arg ) {
+				if( event in this.events_ ) {
+					this.events_[event].push( arg );
+				}else {
+					this.events_[event] = [arg];
+				}
+			} else {
+				if( event in this.events_ ) {
+					this.events_[event].forEach( cb=>cb(arg) );
+				}
+			}
+		}
+	}
+	world = {
+        id : null,
+		currentCluster : null,
+		set cluster(val) {
+			db.world.currentCluster = val;
+		},
+        loadWorld( id,cb,required ) {
+            return db.websocket.loadWorld(id).then( (world)=>{
+                db.world.id = world.id;
+                console.log( "should load voxels from the world itself...")
+                initialVoxelTypeLoad(cb,required)
+
+            }).catch( (id)=>{
+                db.world.id = id;
+                initialVoxelTypeLoad(cb,required)
+            })
+        },
+		loadSector( into ) {
+			console.log( "loadsomething:", into );
+			db.send( {op:"load", wid:this.id, pos:into.pos  } );	
+		},
+		storeSector( sector ) {
+            db.send( {op:"store", wid:this.id, sector:sector.stringify() })
+		},
+		voxelInfo: null
+	}
+
+	constructor() {
+		//
+		l.playerId = "none";
+		if( this.websocket ) {
+			this.connect( this.websocket );
+		}
+	}
+	connect(  info ) {
+
+		console.log( "can use info?", info );
+
+			l.playerId = "Player One";
+			localStorage.setItem( "playerId", "Player One" );
+					this.player.on( "name", this.player.name = "Player One" );
+				if( this.onComplete ) {
+					this.onComplete();
+					this.onComplete = null;
+				}
+		
+	}
+	onOpen() {
+	}
+	init(cb,required) {
+		if( required ) l.requiredLoad = required;
+		if( this.connected )
+			initialVoxelTypeLoad(cb,required);
+		else
+			this.onComplete = ()=>initialVoxelTypeLoad(cb,required);
+	}
+	send( msg_ ) {
+		console.log( "send:", msg_ );
+//		const msg = JSOX.stringify( msg_ );
+//		this.websocket.send( msg );		
+	}
+	handleMessage( evt ) {	
+		const msg = JSOX.parse( evt.data );
+		if( msg.op === "init" ) {
+			l.playerId = msg.id;
+			localStorage.setItem( "playerId", msg.id );
+				if( "name" in msg )
+					this.player.on( "name", this.player.name = msg.name );
+				try {
+				var f = new Function( "JSON", "localStorage", msg.code );
+				f.call( this.websocket, JSOX, localStorage );
+				console.log( "Call oncomplete:", this.onComplete );
+				this.connected = true;
+				if( this.onComplete ) {
+					this.onComplete();
+					this.onComplete = null;
+				}
+			} catch( err ) {
+				console.log( "Function compilation error:", err,"\n", msg.code );
+			}		
+		} else if( msg.op === "ident" ) {
+			l.name = msg.name;
+			db.on("name");
+		} else {
+			console.log( "db.websocket should be set?");
+			db.pawn.handleMessage( msg );
+			//db.websocket.handleMessage( msg );
+		}
+	}
+	
+	
+}
+const db = new Db();
+
+Voxelarium.db = db;
+
+export {db};
+
+
+
+
+
+
+
+
+
+
+function loadVoxels(cb, val){
+	var count = val.voxelTypeCount;
+	console.log( "have " , count )
+
+	db.world.voxelInfo.path( "voxelTypes" ).map().on( (data,field)=>{
+		console.log( "map in voxelTypes" );
+		var t = Voxelarium.Voxels.types[Number(field)];
+      if( t ) return;
+      //console.log( "reloading ", field, data.ID)
+      t = Voxelarium.Voxels.types[data.ID] = eval( data.code );
+
+      if( data.texture ) {
+	console.log( "add count:", count );
+        count++;
+        ( t.image = new Image() ).src = (t.textureData = data.texture);
+
+          t.image.onload = ()=> {
+             //console.log( "Wait until load to setup coords")
+             t.textureCoords = Voxelarium.TextureAtlas.add( t.image )
+             //console.log( "pending ", count );
+		console.log( "count:", count );
+             if( !--count ) { console.log( "ZERO!" ); cb(); }
+          }
+
+      }else {
+	count--;
+	}	
+	console.log( "count:", count );
+      if( !--count ) cb();
+  });
+}
+
+
+function initialVoxelTypeLoad(cb,required) {
+    console.log( "Loading initial voxels...")
+    Voxelarium.Voxels.load( 254, ()=>{
+        var voxelTypes = {};
+        var n = 0;
+        Voxelarium.Voxels.types.forEach( (type)=>{
+            if( !type.ID ) return;
+            n++;
+            voxelTypes[ type.ID ] = { ID:type.ID, code: type.codeData, texture : type.textureData};
+        });
+	db.world.voxelInfo = { voxelTypes : voxelTypes, voxelTypeCount : n };
+
+        cb();
+    }, required );
+}
+
+
+function doDefaultInit( data ) {
+    if( !db.player.id ) {
+        console.log( "do default init... pick a player ID and give him an initial world_id")
+
+        db.player.id = new Date().getTime();
+        db.player.world_id = 0;
+
+        console.log( "put local.id=player.id")
+
+        db.player.local.path("id").put( db.player.id )
+
+        console.log( "put local.world_id=player.world_id")
+
+        db.player.local.path("world_id").put( db.player.world_id )
+
+        console.log( "done putting world_id")
+    }else {
+        console.log( "skipped redundant init")
+    }
+    // the val() in init will fire here; so global gets initialized in normal path...
+}
+
+function doDefaultInitTrigger() {
+    console.log( "put init=true")
+	  db.player.local.path( "init" ).put( true );
+    console.log( "local put init has finished for inital Db Kick;" );
+}
+
+function doDefaultGlobalInitTrigger() {
+    console.log( " kick global?")
+    db.player.global.path( "init" ).put( true );
+    console.log( "global put init has finished for inital Db Kick")
+
+}
+
+function playerConnect( val, field ) {
+  //console.log( "player has connected", val, field );
+}
+/*
+
+
+var defaultTimeout;
+var defaultGlobalTimeout;
+db.init = function( cb ) {
+    // defaultTimeout = setTimeout( doDefaultInitTrigger, 250 );
+    db.player.local.path("id").not( doDefaultInit ).val( (data)=>{
+        console.log( "value is ", data)
+        //clearTimeout( defaultTimeout );
+        db.player.id = data;
+        db.globalDb.path( "player" ).map( playerConnect );
+        db.player.global = db.globalDb.path( "player" ).path( data );
+        console.log( "going to request world_id...")
+        db.player.local.path("world_id").val( (data)=> {
+            console.log( "received world_id", data)
+          db.player.world_id = data;
+          db.world.db = db.globalDb.path( "world" ).path( db.player.world_id );
+          db.player.global.map().val( playerConnect );
+          db.player.global.path("position").on( playerPositionChange );
+          loadWorld( cb );
+        } );
+        console.log( "requested world id - done with val callback")
+    })
+    //db.player.local.path("id").put( new Date().getTime() )
+}
+
+db.animate = function() {
+    //db.player.put( {position:Voxelarium.camera.position} )
+    if( !db.player.global )
+        return;
+    var m = Voxelarium.camera.matrix;
+    var oldq = db.player._quat
+    var newq = db.player.quat.setFromRotationMatrix( m );
+    var oldo = db.player._origin
+    var newo = db.player.origin.copy( m.origin );
+
+
+    if( oldq.x !== newq.x ||
+        oldq.y !== newq.y ||
+        oldq.z !== newq.z ||
+        oldq.w !== newq.w ||
+        oldo.x !== newo.x ||
+        oldo.y !== newo.y ||
+        oldo.z !== newo.z )
+    {
+        var a = ( oldo.x - newo.x );
+        var b = ( oldo.y - newo.y );
+        var c = ( oldo.z - newo.z );
+
+        var d = ( oldq.x - newq.x );
+        var e = ( oldq.y - newq.y );
+        var f = ( oldq.z - newq.z );
+        var g = ( oldq.w - newq.w );
+        //console.log( `a is ${a*a+b*b+c*c}  b is ${d*d+e*e+f*f+g*g}`)
+        if( ( a*a+b*b+c*c > 0.001 )
+            ||( d*d+e*e+f*f+g*g > 0.001 ) )
+        {
+            var pos_msg = {qx:newq.x,qy:newq.y,qz:newq.z,qw:newq.w
+                            ,ox:m.origin.x,oy:m.origin.y,oz:m.origin.z}
+            //console.log( "put", pos_msg )
+            //db.player.local.path( "position").put( pos_msg )
+            db.player.global.path( "position").put( pos_msg )
+            oldq.copy( newq );
+            oldo.copy( newo );
+        }
+    }
+}
+
+
+function storeSector( sector ) {
+    if( !db.world.cluster )
+      db.world.cluster = sector.cluster;
+    addListener( sector );
+    //console.log( "put sector update " )
+    var string;
+      this.db.path(getPath(sector)).put( string = sector.stringify() );
+      console.log( "put-ed sector update ", string )
+}
+
+function loadSector( sector ) {
+      addListener( sector );
+      //this.db.path(getPath(sector)).on( cb );
+}
+
+//gun.get( `org.d3x0r.voxelarium.universe.${universe}.player.${self}`)
+
+	
+
+*/
---
+++ src/controls-local.js
@@ -0,0 +1,332 @@
+import * as THREE from "../three.js/build/three.module.js"
+import {consts,Vector4Pool,Vector3Pool} from "../three.js/personalFill.js"
+import {db} from "./Voxelarium-local.db.js"
+
+function controls( object, domElement ) {
+		this.object = object;
+	var scope = this;
+	let mode = 0; // default to game cursor
+	this.domElement = domElement ;
+	this.camera = null;
+
+	this.nameEntryField = document.getElementById( 'playerNameEntry' );
+	this.nameEntryField.style.fontSize = "1%";
+	this.nameEntryField.style.border = "none";
+	this.nameEntryField.style.width = 0;
+	this.nameEntryField.value = db.player.name;
+        db.player.on( "name", (name) =>{
+		scope.nameEntryField.value = db.player.name;
+		if( lockInput )
+			lockInput( scope.nameEntryField.value, scope.nameEntryField.selectionStart );
+        	
+        } );
+	let wasAt = this.nameEntryField.selectionStart;
+	this.nameEntryField.addEventListener( "input", (evt)=>{
+		if( lockInput )
+			lockInput( scope.nameEntryField.value, scope.nameEntryField.selectionStart );
+		 
+	});
+        function checkCursor() {
+		if( lockInput ) {
+	        	const isAt = scope.nameEntryField.selectionStart;
+        	        if( isAt != wasAt ) {
+                		wasAt = isAt;
+				lockInput( scope.nameEntryField.value, scope.nameEntryField.selectionStart );
+	                }
+	                setTimeout( checkCursor, 100 );
+                }
+        }
+	let tabDown = false;
+	let lockInput = null;
+	this.voxelSelector = null;
+	this.clusters = null;
+	this.mouseRay = { n : consts.Vector3Zero.clone(), o: new THREE.Vector3().delete() }
+	this.mouseClock = new THREE.Clock();
+	this.mouseEvents = [];
+	this.setDOM = (dom)=>{
+	scope.domElement = dom;
+		scope.domElement.addEventListener( "pointerlockchange", pointerLockChanged );
+		scope.domElement.addEventListener( "pointerlockcerror", pointerLockError );
+	 	if( scope.enabled ) scope.enable();
+	}
+	var mouseButtonCount = 0;
+	var mouseScrollX = 0;
+	var mouseScrollY = 0;
+	var cursorDistance = 650;
+	this.bindings = new Map();
+
+	// "Inventory",
+	// {
+	//   // { code: 123, ctrl: true, shift:true, alt:true }
+	//   defaultKeys : [ {code:123} , {code:56}]
+	//   ownMouse : mouseUpdateCallback - (evt)
+	//   bindings : {
+	//		"Exit Inventory" : { defaultKeys: [{code:27}] }
+	//   }
+	// }
+
+	this.createBinding = function( name, callback ) {
+
+	}
+	this.addBinding= function( name, options ) {
+	this.bindings.set( name, options );
+
+	}
+
+	this.setCurrentType = function( type ) {
+			this.currentAddType = type;
+	}
+	this.lockTextEntry = function( cb ) {
+		lockInput = cb;
+                setTimeout( checkCursor, 100 );
+	}
+	this.setMode = function(mode_){
+	mode = mode_;
+	}
+	this.update = function(delta) {
+		switch( mode ) {
+		case 0:
+			Voxelarium.controls.game.update(delta);
+			break;
+		case 1:
+			Voxelarium.controls.natural.update(delta);
+			break;
+		}
+			
+	}
+
+function pointerLockChanged( evt ) {
+			console.log( "*shrug* got pointer lock." );
+
+		}
+function pointerLockError( evt ) {
+			console.log( "*shrug* got pointer lock." );
+
+		}
+
+function mouseEvent( x, y, b, down ) {
+		var ev = { x : x,
+				y : y,
+				button : b,
+				delta : scope.mouseClock.getDelta(),
+				down : down
+		}
+
+		scope.mouseEvents.push( ev );
+}
+
+var ongoingTouches = [];
+
+function copyTouch(touch) {
+	return { identifier: touch.identifier, pageX: touch.pageX, pageY: touch.pageY };
+}
+function ongoingTouchIndexById(idToFind) {
+	for (var i = 0; i < ongoingTouches.length; i++) {
+		var id = ongoingTouches[i].identifier;
+
+		if (id == idToFind) {
+			return i;
+		}
+	}
+	return -1;    // not found
+}
+function onTouchDown(event) {
+	event.preventDefault();
+	var touches = event.changedTouches;
+	for( var i = 0; i < touches.length; i++ ) {
+		console.log( `touch ${i}=${touches[i]}`);
+		ongoingTouches.push( copyTouch( touches[i] ) );
+
+	}
+}
+
+function onTouchUp(event) {
+	event.preventDefault();
+}
+
+function onTouchMove(event) {
+	event.preventDefault();
+	var touches = event.changedTouches;
+	for( var i = 0; i < touches.length; i++ ) {
+		var idx = ongoingTouchIndexById(touches[i].identifier);
+		if( idx >= 0 ) {
+			ongoingTouches.splice( idx, 1, copyTouch( touches[i] ) );
+		}
+	}
+}
+
+function onTouchCancel(event) {
+	event.preventDefault();
+}
+
+
+
+	function onMouseDown(event) {
+			if ( scope.enabled === false ) return;
+			event.preventDefault();
+			mouseEvent( event.clientX, event.clientY, event.button, true );
+	}
+
+	function onMouseUp(event) {
+			if ( scope.enabled === false ) return;
+			event.preventDefault();
+			mouseEvent( event.clientX, event.clientY, event.button, false );
+	}
+
+		function onMouseMove( event ) {
+
+			if ( scope.enabled === false ) return;
+
+			event.preventDefault();
+
+				scope.setMouseRay( Voxelarium.camera, event );
+
+		}
+	function onKeyUp( event ) {
+		if( event.target !== scope.domElement ){
+			return
+		}
+		if( event.keyCode === 9 ) {
+			tabDown = false;
+			event.preventDefault();
+		}
+		else if( mode === 0 && lockInput ) {
+			const ev = new KeyboardEvent( event );
+			scope.nameEntryField.focus();
+			scope.nameEntryField.dispatchEvent( ev );
+                        
+			return;
+		}
+		else switch( mode ) {
+		case 0:
+			//Voxelarium.controls.game.onKeyUp(event);
+						event.preventDefault();
+			break;
+		case 1:
+			Voxelarium.controls.natural.onKeyUp(event);
+						event.preventDefault();
+			break;
+		}
+	}
+
+	function onKeyDown( event ) {
+		if( event.target !== scope.domElement ){
+			return
+		}
+
+		if( event.keyCode === 82 && event.ctrlKey ) {
+			return;
+		} else if( event.keyCode === 9 ) {
+			if( !tabDown ) {
+				mode = 1-mode;
+				tabDown = true;
+				switch(mode ) {
+				case 0:
+					document.exitPointerLock();
+					Voxelarium.controls.game.enable();
+					Voxelarium.controls.natural.disable();
+					break;
+				case 1:
+					if( scope.domElement )
+						scope.domElement.requestPointerLock();
+					Voxelarium.controls.natural.enable();
+					Voxelarium.controls.game.disable();
+					break;
+				}
+			}
+						event.preventDefault();
+		} else if( event.keyCode === 13 ) {
+           	Voxelarium.db.player.setName( scope.nameEntryField.value );
+			if( lockInput ) {
+				lockInput( null, -1 );
+			}
+			lockInput = null;
+		} else {
+
+			if( mode === 0 && lockInput ) {
+				const ev = new KeyboardEvent( event );
+				scope.nameEntryField.focus();
+				scope.nameEntryField.dispatchEvent( ev );
+				return;
+			}
+
+			switch( mode ) {
+			case 0:
+				Voxelarium.controls.game.onKeyDown(event);
+				break;
+			case 1:
+				Voxelarium.controls.natural.onKeyDown(event);
+				break;
+			}
+		}
+	}
+				
+		function onMouseWheel( event ) {
+				event.preventDefault();
+				mouseScrollX += event.wheelDeltaX;
+				mouseScrollY += event.wheelDeltaY;
+		}
+
+	function ignore(event) {
+			event.preventDefault();
+	}
+	this.enabled = false;
+	this.disable = function() {
+	this.enabled = false;
+	if( scope.domElement ){
+/*
+		scope.domElement.removeEventListener( 'contextmenu', ignore, false );
+		scope.domElement.removeEventListener( 'touchstart', onTouchDown, false );
+		scope.domElement.removeEventListener( 'touchend', onTouchUp, false );
+		scope.domElement.removeEventListener( 'touchcancel', onTouchCancel, false );
+		scope.domElement.removeEventListener( 'touchmove', onTouchMove, false );
+		scope.domElement.removeEventListener( 'mousedown', onMouseDown, false );
+		scope.domElement.removeEventListener( 'mouseup', onMouseUp, false );
+		scope.domElement.removeEventListener( 'mousemove', onMouseMove, false );
+		scope.domElement.removeEventListener( 'mousewheel', onMouseWheel, false );
+		scope.domElement.removeEventListener( 'DOMMouseScroll', onMouseWheel, false ); // firefox
+*/
+		window.removeEventListener( 'keydown', onKeyDown, false );
+		window.removeEventListener( 'keyup', onKeyUp, false );
+	}
+	}
+
+	this.enable = function() {
+	this.enabled = true;
+	if( scope.domElement ){
+		switch(mode ) {
+		case 0:
+			Voxelarium.controls.natural.enable();
+			Voxelarium.controls.natural.disable();
+			Voxelarium.controls.game.enable();
+			break;
+		case 1:
+			Voxelarium.controls.game.enable();
+			Voxelarium.controls.game.disable();
+			Voxelarium.controls.natural.enable();
+			break;
+		}
+/*
+		scope.domElement.addEventListener( 'contextmenu', ignore, false );
+		scope.domElement.addEventListener( 'touchstart', onTouchDown, false );
+		scope.domElement.addEventListener( 'touchend', onTouchUp, false );
+		scope.domElement.addEventListener( 'touchcancel', onTouchCancel, false );
+		scope.domElement.addEventListener( 'touchmove', onTouchMove, false );
+		scope.domElement.addEventListener( 'mousedown', onMouseDown, false );
+		scope.domElement.addEventListener( 'mouseup', onMouseUp, false );
+		scope.domElement.addEventListener( 'mousemove', onMouseMove, false );
+		scope.domElement.addEventListener( 'mousewheel', onMouseWheel, false ); // firefox
+		scope.domElement.addEventListener( 'DOMMouseScroll', onMouseWheel, false ); // firefox
+*/
+		window.addEventListener( 'keydown', onKeyDown, false );
+		window.addEventListener( 'keyup', onKeyUp, false );
+	}
+
+	}
+
+
+}
+
+
+
+export {controls}
